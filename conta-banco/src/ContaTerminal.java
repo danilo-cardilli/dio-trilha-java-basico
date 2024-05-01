dbea9077245e6ca548b6f8f426b9b87e5bf79222
Danilo Cardilli <danilocardilli@yahoo.com.br>
2024-05-01 12:21:24 -0300

    Projeto completo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -37,5 +37,6 @@ public class ContaTerminal {
         // Exibir a mensagem conta criada
 
         System.out.println("\nOlá ["+ nomeCliente +"], obrigado por criar uma conta em nosso banco, sua agência é ["+ agencia +"], conta ["+ numero +"] e seu saldo ["+ saldo +"] já está disponível para saque");
+        scanner.close();
     }
 }
